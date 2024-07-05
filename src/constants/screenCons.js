591ab181ce615aa54573bc207f5f9e1ccca799b6
demoaccount-rishav <rishavbhowmick.2002.va@gmail.com>
2024-07-05 11:34:30 +0530

    setup constants

---
+++ src/constants/screenCons.js
@@ -0,0 +1,10 @@
+import { Dimensions } from "react-native";
+
+const { height, width } = Dimensions.get('window');
+
+export const widthPercentage = p => {
+    return (p * width) / 100;
+}
+export const heightPercentage = p => {
+    return (p * height) / 100;
+}
